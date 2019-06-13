abe6bb71a37dd5c982bf6351596267882df024d9
League Student <league@iMac.attlocal.net>
2019-06-12 19:56:08 -0700

    almost donw with module 5

--- src/nested_loops/SkillPracticeWorksheet.java
+++ src/nested_loops/SkillPracticeWorksheet.java
@@ -28,12 +28,23 @@ int mixednumber2 = Integer.parseInt(number2);
 int difference = mixednumber1 - mixednumber2;
 JOptionPane.showMessageDialog(null, difference);
 String city = JOptionPane.showInputDialog("Which city do YOU currently live in?");
-int hometown = Integer.parseInt(city);
-if (city == "San Diego") {
-JOptionPane.showMessageDialog(null, "YOU live in America's finest city!");
+if (city .equalsIgnoreCase("San Diego")) {
+JOptionPane.showMessageDialog(null, "YOU live in America's finest city");
 }
 else {
 JOptionPane.showMessageDialog(null, "YOU SHOULD really move to San Diego");	
 }
+String transportation = JOptionPane.showInputDialog("How many cars does your family have? This will tell you number of wheels your family has on their car.");
+int wheels = Integer.parseInt(transportation);
+if(wheels == 0) {
+JOptionPane.showMessageDialog(null, "I bet your family uses public transportation");
+}
+int carWheels = wheels * 4;
+if (wheels > 0) {
+JOptionPane.showMessageDialog(null, carWheels);
+}
+String school = JOptionPane.showInputDialog("What school are you going to next year?");
+JOptionPane.showMessageDialog(null, school);
+JOptionPane.showMessageDialog(null, "That is a fantastic school! You should be very excited to go there next year!");
 }
 }
